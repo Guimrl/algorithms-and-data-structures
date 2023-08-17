049e020d8ff68d9325b5c5339b661ee707d34b6c
Guilherme Amaral <gui.amaral123@outlook.com>
2023-08-17 00:03:36 +0000

    bubbleSort

--- src/algorithms/bubbleSort.js
+++ src/algorithms/bubbleSort.js
@@ -1,11 +1,13 @@
-const bubbleSort = (arr) => {
-  for (let j = 0; j < arr.length; j++) {
-    for (let i = 0; i < arr.length; i++) {
-      if(arr[i] > arr[i+1]) {
-        [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+const bubbleSort = (array) => {
+  for (let j = 0; j < array.length; j++) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > array[i + 1]) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
       }
     }
   }
-  return arr;
+
+  return array;
 }
 
+export default bubbleSort;
